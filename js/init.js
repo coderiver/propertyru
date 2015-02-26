ad43a3f0688bdecd867b20e612a84f2b4b48a2b6
andreyka_g <andreyka_g@ukr.net>
2015-02-26 17:47:51 +0200

    upd

--- js/init.js
+++ js/init.js
@@ -1,5 +1,6 @@
 head.load("js/lib/jquery.js", 
 					"js/lib/cycle.js",
+					"js/lib/chosen.jquery.js",
 					"js/lib/cycle.carousel.js",
 					"js/lib/slick.min.js",
 					"js/jquery.content_slider.min.js",
